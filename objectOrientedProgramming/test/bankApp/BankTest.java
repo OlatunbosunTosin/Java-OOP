c0e2d2c2b87f05e9b54abe21030486bb64e3dbec
Tosin <adeolamimilabake@gmail.com>
2026-07-06 14:55:35 +0100

    account

--- objectOrientedProgramming/test/bankApp/BankTest.java
+++ objectOrientedProgramming/test/bankApp/BankTest.java
@@ -9,72 +9,74 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class BankTest {
 
     private Bank myBank;
+    private String pin;
 
     @BeforeEach
     public void setUp(){
         myBank = new Bank();
+        pin = "mypin";
     }
 
     @Test
     public void register1Customer_1AccountIsCreated(){
-        myBank.registerCustomer("tos","ola", "pin");
+        myBank.registerCustomer("tos","ola", pin);
         assertEquals(1, myBank.getNumberOfAccounts());
     }
 
     @Test
     public void register2Customers_2AccountsAreCreated(){
-        myBank.registerCustomer("tos","ola", "pin");
+        myBank.registerCustomer("tos","ola", pin);
         assertEquals(1, myBank.getNumberOfAccounts());
-        myBank.registerCustomer("hi","bye", "pin");
+        myBank.registerCustomer("ayo","mide", pin);
         assertEquals(2, myBank.getNumberOfAccounts());
     }
 
     @Test
     public void registerTwoAccounts_findAccountOfOneAccountTest(){
-        Account customerOne = myBank.registerCustomer("tos","ola", "pin");
-        Account customerTwo = myBank.registerCustomer("hi","bye", "pin");
+        Account customerOne = myBank.registerCustomer("tos","ola", pin);
+        Account customerTwo = myBank.registerCustomer("ayo","mide", pin);
         assertEquals(customerTwo,myBank.findAccount("2"));
     }
 
     @Test
     public void registerTwoAccounts_findAccountThatDoesNotExistTest(){
-        Account customerOne = myBank.registerCustomer("tos","ola", "pin");
-        Account customerTwo = myBank.registerCustomer("hi","bye", "pin");
+        Account customerOne = myBank.registerCustomer("tos","ola", pin);
+        Account customerTwo = myBank.registerCustomer("ayo","mide", pin);
         assertThrows(IllegalArgumentException.class, () -> myBank.findAccount("3"));
     }
 
     @Test
     public void deposit5000ToAccount2Test(){
-        Account customerOne = myBank.registerCustomer("tos","ola", "pin");
-        Account customerTwo = myBank.registerCustomer("hi","bye", "pin");
+        Account customerOne = myBank.registerCustomer("tos","ola", pin);
+        Account customerTwo = myBank.registerCustomer("ayo","mide", pin);
         myBank.deposit("2", 5000);
-        assertEquals(5000,myBank.checkBalance("2", "pin"));
-        assertEquals(0,myBank.checkBalance("1", "pin"));
+        assertEquals(5000,myBank.checkBalance("2", pin));
+        assertEquals(0,myBank.checkBalance("1", pin));
     }
 
     @Test
     public void deposit5000_withdraw2000_balaceIs3000Test(){
-        Account customerOne = myBank.registerCustomer("tos","ola", "pin");
-        Account customerTwo = myBank.registerCustomer("hi","bye", "pin");
+        Account customerOne = myBank.registerCustomer("tos","ola", pin);
+        Account customerTwo = myBank.registerCustomer("ayo","mide", pin);
         myBank.deposit("2", 5000);
-        myBank.withdraw("2", 2000, "pin");
-        assertEquals(3000,myBank.checkBalance("2", "pin"));
+        myBank.withdraw("2", 2000, pin);
+        assertEquals(3000,myBank.checkBalance("2", pin));
     }
 
     @Test
     public void deposit5000_transfer2500ToAnotherAccount(){
-        Account customerOne = myBank.registerCustomer("tos","ola", "pin");
-        Account customerTwo = myBank.registerCustomer("hi","bye", "pin");
+        Account customerOne = myBank.registerCustomer("tos","ola", pin);
+        Account customerTwo = myBank.registerCustomer("ayo","mide", pin);
         myBank.deposit("1", 5000);
-        myBank.transfer("1", "2", 2500, "pin");
-        assertEquals(2500,myBank.checkBalance("2", "pin"));
+        myBank.transfer("1", "2", 2500, pin);
+        assertEquals(2500,myBank.checkBalance("2", pin));
     }
 
     @Test
     public void removeAccountTest(){
-        Account customerOne = myBank.registerCustomer("tos","ola", "pin");
-        Account customerTwo = myBank.registerCustomer("hi","bye", "pin");
-        myBank.removeAccount("2", "pin");
+        Account customerOne = myBank.registerCustomer("tos","ola", pin);
+        Account customerTwo = myBank.registerCustomer("ayo","mide", pin);
+        myBank.removeAccount("2", pin);
         assertEquals(1,myBank.getNumberOfAccounts());
     }
 }
